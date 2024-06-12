3a858d5b88424c69784cf06cc2e8b39ba4e7dc43
ej-shafran <ejshafran@hilma.tech>
2024-06-12 17:39:27 +0300

    Add crzhash tests

---
+++ test/crzhash.c
@@ -0,0 +1,91 @@
+#include "crzhash.h"
+#include "crztest.h"
+
+#define DUMP(name, ht)                       \
+	do {                                 \
+		TEST_LOG("%s = ", name);     \
+		HASH_TABLE_DEBUG(ht, "%zu"); \
+	} while (0)
+
+static HASH_TABLE(size_t) ht = HASH_TABLE_NEW();
+
+void cleanup(void)
+{
+	HASH_TABLE_FREE(&ht);
+}
+
+TEST_MAIN({
+	AFTER_EACH(cleanup);
+
+	DESCRIBE("HASH_TABLE_INSERT", {
+		TEST("Inserting a key-value pair", {
+			// Arrange
+			HASH_TABLE_INIT(&ht, 2);
+
+			// Act
+			HASH_TABLE_INSERT(&ht, "a", 1);
+
+			// Assert
+			HASH_PAIR(size_t) *result = HASH_TABLE_GET(ht, "a");
+			EXPECT(result != CRZ_NULL && result->value == 1);
+		});
+
+		TEST("Properly inserting with collision", {
+			// Invariant
+			EXPECT(crz__djb2_hash("ab") % 2 ==
+			       crz__djb2_hash("ba") % 2);
+
+			// Arrange
+			HASH_TABLE_INIT(&ht, 2);
+
+			// Act
+			HASH_TABLE_INSERT(&ht, "ab", 1);
+			HASH_TABLE_INSERT(&ht, "ba", 2);
+
+			// Assert
+			HASH_PAIR(size_t) *result = HASH_TABLE_GET(ht, "ab");
+			EXPECT(result != CRZ_NULL && result->value == 1);
+		});
+
+		TEST("Properly inserting with overflow", {
+			// Arrange
+			HASH_TABLE_INIT(&ht, 2);
+			CRZ_SIZE original = ht.size;
+
+			// Act
+			HASH_TABLE_INSERT(&ht, "a", 1);
+			HASH_TABLE_INSERT(&ht, "b", 2);
+			HASH_TABLE_INSERT(&ht, "c", 3);
+
+			// Assert
+			HASH_PAIR(size_t) *result = HASH_TABLE_GET(ht, "a");
+			EXPECT(result != CRZ_NULL && result->value == 1);
+			EXPECT(ht.size == original * 2);
+		});
+	});
+
+	DESCRIBE("HASH_TABLE_GET", {
+		TEST("Returning value", {
+			// Arrange
+			HASH_TABLE_INIT(&ht, 2);
+			HASH_TABLE_INSERT(&ht, "a", 1);
+
+			// Act
+			HASH_PAIR(size_t) *result = HASH_TABLE_GET(ht, "a");
+
+			// Assert
+			EXPECT(result != CRZ_NULL && result->value == 1);
+		});
+
+		TEST("Returning CRZ_NULL when nonexistent", {
+			// Arrange
+			HASH_TABLE_INIT(&ht, 2);
+
+			// Act
+			HASH_PAIR(size_t) *result = HASH_TABLE_GET(ht, "a");
+
+			// Assert
+			EXPECT(result == CRZ_NULL);
+		});
+	});
+})
